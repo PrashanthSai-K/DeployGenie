9de6893bf61d096ce024d5f377801bd51ce141b5
PrashanthSai-K <saiprashanth.cs21@bitsathy.ac.in>
2024-09-26 13:58:29 +0530

    Admin module/User Module management done

--- api/src/routes/gitlab/gitlab.go
+++ api/src/routes/gitlab/gitlab.go
@@ -1,16 +1,2 @@
 package gitlabRoutes
 
-import (
-	gitlabHandler "github.com/PrashanthSai-K/DeployGenie/api/src/handlers/gitlab"
-	"github.com/gofiber/fiber/v2"
-)
-
-
-func SetUpGitlsbRoutes (router fiber.Router){
-
-	gitlab := router.Group("/gitlab")
-
-	gitlab.Get("/", gitlabHandler.GetUsers);
-
-	gitlab.Get("/project", gitlabHandler.CreateProject)
-}
